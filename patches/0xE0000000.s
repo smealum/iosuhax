b277c01bbffa44ffd475e442426d55b3f3efbcad
Maschell <Maschell@gmx.de>
2016-11-09 00:03:19 +0100

    removed hash check of boot logos (allows cutom logos)

---
+++ patches/0xE0000000.s
@@ -0,0 +1,11 @@
+.arm.big
+
+.open "sections/0xE0000000.bin","patched_sections/0xE0000000.bin",0xE0000000
+
+; allow custom bootLogoTex and bootMovie.h264
+.org 0xE0030D68
+	mov r0, #0
+.org 0xE0030D34
+	mov r0, #0
+
+.Close
