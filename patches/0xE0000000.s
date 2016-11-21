a20cb2e559cbef0a58d7e078cd4b3cb376834b65
Marco <marco.calautti@gmail.com>
2016-11-21 18:17:21 +0000

    Fixed regionfree patch, now works .-.

--- patches/0xE0000000.s
+++ patches/0xE0000000.s
@@ -8,10 +8,13 @@
 .org 0xE0030D34
 	mov r0, #0
 
+.org 0xE0030498
+    mov r0, #0
+    
 ; allow region free titles
-.org 0xE0030DD8
-    mov r0, #0
-.org 0xE0030EB8
-    mov r0, #0
+;.org 0xE0030DD8
+ ;   mov r0, #0
+;.org 0xE0030EB8
+;    mov r0, #0
     
 .Close
